9b77050a5d1151692adf275821b6d2eec795e132
Ari's Laptop <151627853+ayshe811@users.noreply.github.com>
2024-01-31 15:09:20 +0000

    Uploaded Character Assets

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -12,7 +12,7 @@ public class playerController : MonoBehaviour
     private bool playerMove;
     public float jumpForce;
 
-    Rigidbody rb;
+    public Rigidbody rb;
     public  Camera cam;
     // Start is called before the first frame update
     void Start()

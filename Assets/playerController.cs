3a0472b55c95a0d980d614818fd3cc9c331a327b
Ari's Laptop <151627853+ayshe811@users.noreply.github.com>
2024-01-31 19:02:32 +0000

    Added new assets

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -12,20 +12,42 @@ public class playerController : MonoBehaviour
     private bool playerMove;
     public float jumpForce;
 
-    public Rigidbody rb;
-    public  Camera cam;
+    Rigidbody rb;
+    public Camera cam;
 
+    Animator anim;
     [SerializeField] private bool isMoving;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         playerSpeed = 250;
+
+        anim = GetComponent<Animator>();
     }
 
     public void FixedUpdate()
     {
-        // player rotation
+        if (Input.GetKey(KeyCode.Space)) rb.AddForce(Vector2.up * jumpForce);
+
+        animations();
+        rotation();
+        movement();
+    }
+
+    private void animations()
+    {
+        // move
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("move", true);
+            isMoving = true;
+        }
+        else anim.SetBool("move", false);
+    }
+    private void rotation()
+    {
         mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * xSensitivity;
         transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
 
@@ -35,8 +57,9 @@ public class playerController : MonoBehaviour
         //  xRotation -= mouseY; // -= bcos i want the rotation along the x axis to correspond with my mouse position (up and down)
         //  xRotation = Mathf.Clamp(xRotation, -80, 80);
         //   cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-
-        // player movement
+    }
+    private void movement()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
         zInput = Input.GetAxisRaw("Vertical");
 
@@ -44,8 +67,5 @@ public class playerController : MonoBehaviour
 
         Vector3 move = (xInput * transform.right) + (zInput * transform.forward); // setting direction
         rb.velocity = new Vector3(move.x * (playerSpeed * Time.deltaTime), rb.velocity.y, move.z * (playerSpeed * Time.deltaTime));
-
-        // jump mechanic
-        if (Input.GetKey(KeyCode.Space)) rb.AddForce(Vector2.up * jumpForce);
     }
 }
